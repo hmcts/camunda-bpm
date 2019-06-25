b506057ca4b96dcf63fea3401c88cbed97d4e67d
Tim Jacomb <tim.jacomb@hmcts.net>
2019-06-25 16:50:36 +0100

    Set profile information properly

--- src/main/java/com/camunda/demo/filter/webapp/SpringSecurityAuthenticationProvider.java
+++ src/main/java/com/camunda/demo/filter/webapp/SpringSecurityAuthenticationProvider.java
@@ -1,18 +1,25 @@
 package com.camunda.demo.filter.webapp;
 
+import java.util.Collection;
+import java.util.Map;
+import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.identity.User;
 import org.camunda.bpm.engine.rest.security.auth.AuthenticationResult;
 import org.camunda.bpm.engine.rest.security.auth.impl.ContainerBasedAuthenticationProvider;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 
 public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenticationProvider {
 
     @Override
+    @SuppressWarnings("unchecked")
     public AuthenticationResult extractAuthenticatedUser(HttpServletRequest request, ProcessEngine engine) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -26,8 +33,23 @@ public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenti
             return AuthenticationResult.unsuccessful();
         }
 
-        AuthenticationResult authenticationResult = new AuthenticationResult(name, true);
+        List<OidcUserAuthority> authorities = (List<OidcUserAuthority>) authentication.getAuthorities();
+
+        Map<String, Object> attributes = authorities.get(0).getAttributes();
+        AuthenticationResult authenticationResult = new AuthenticationResult(
+                name,
+                true
+        );
         authenticationResult.setGroups(getUserGroups(authentication));
+
+        IdentityService identityService = engine.getIdentityService();
+        User user = identityService.newUser(name);
+        user.setFirstName(attributes.get("given_name").toString());
+        user.setLastName(attributes.get("family_name").toString());
+        user.setEmail(attributes.get("upn").toString());
+        
+        identityService.deleteUser(name);
+        identityService.saveUser(user);
 
         return authenticationResult;
     }
