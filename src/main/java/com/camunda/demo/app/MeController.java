d8f2d02f6ba43ed9f9577968099b6ea6247c2213
Tim Jacomb <tim.jacomb@hmcts.net>
2019-06-25 16:29:40 +0100

    Initial commit

---
+++ src/main/java/com/camunda/demo/app/MeController.java
@@ -0,0 +1,14 @@
+package com.camunda.demo.app;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeController {
+
+    @GetMapping("/me")
+    public OAuth2AuthenticationToken me(OAuth2AuthenticationToken me) {
+        return me;
+    }
+}
