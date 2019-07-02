83184cafeb419b81de84acccd705fc855972797e
Tim Jacomb <tim.jacomb@hmcts.net>
2019-07-02 15:50:07 +0100

    I did stuff

---
+++ src/main/java/uk/gov/hmcts/app/AuthorizationHelper.java
@@ -0,0 +1,141 @@
+package uk.gov.hmcts.app;
+
+import org.camunda.bpm.engine.AuthorizationService;
+import org.camunda.bpm.engine.authorization.Authorization;
+import org.camunda.bpm.engine.authorization.Permissions;
+import org.camunda.bpm.engine.authorization.Resources;
+
+public class AuthorizationHelper {
+
+    private AuthorizationService authorizationService;
+
+    public AuthorizationHelper(AuthorizationService authorizationService) {
+        this.authorizationService = authorizationService;
+    }
+
+    public void deploymentAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.DEPLOYMENT)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.DEPLOYMENT);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void taskAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.TASK)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.TASK);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void processDefinition(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.PROCESS_DEFINITION)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.PROCESS_DEFINITION);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void processInstance(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.PROCESS_INSTANCE)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.PROCESS_INSTANCE);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void batchAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.BATCH)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.BATCH);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void decisionDefinitionAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.DECISION_DEFINITION)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.DECISION_DEFINITION);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void optimiseAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.OPTIMIZE)
+                .groupIdIn(groupId).count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.OPTIMIZE);
+            authorization.setResourceId("*");
+            authorization.setGroupId(groupId);
+
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void cockpitAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.APPLICATION)
+                .groupIdIn(groupId).resourceId("cockpit").count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.setGroupId(groupId);
+
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.APPLICATION);
+            authorization.setResourceId("cockpit");
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+
+    public void tasklistAccess(String groupId) {
+        boolean authExists = authorizationService.createAuthorizationQuery().resourceType(Resources.APPLICATION)
+                .groupIdIn(groupId).resourceId("tasklist").count() > 0;
+        if (!authExists) {
+            Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+            authorization.setGroupId(groupId);
+
+            authorization.addPermission(Permissions.ALL);
+            authorization.setResource(Resources.APPLICATION);
+            authorization.setResourceId("tasklist");
+            authorizationService.saveAuthorization(authorization);
+        }
+    }
+}
--- src/main/java/uk/gov/hmcts/filter/webapp/SpringSecurityAuthenticationProvider.java
+++ src/main/java/uk/gov/hmcts/filter/webapp/SpringSecurityAuthenticationProvider.java
@@ -2,8 +2,11 @@ package uk.gov.hmcts.filter.webapp;
 
 import java.util.ArrayList;
 import java.util.Map;
+import org.camunda.bpm.engine.AuthorizationService;
 import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.authorization.Authorization;
+import org.camunda.bpm.engine.authorization.Resources;
 import org.camunda.bpm.engine.identity.Group;
 import org.camunda.bpm.engine.identity.Tenant;
 import org.camunda.bpm.engine.identity.User;
@@ -15,9 +18,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import uk.gov.hmcts.app.AuthorizationHelper;
 
 import static java.util.Collections.emptyList;
-
 
 @SuppressWarnings("unused")
 public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenticationProvider {
@@ -26,11 +29,16 @@ public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenti
     private static final String GLOBAL_ADMIN_GROUP = "44886fcb-4564-4bf9-98a5-4f7629078223";
     private static final String CMC_TENANT_ID = "cmc";
     private static final String CAMUNDA_ADMIN_GROUP = "camunda-admin";
-    public static final String CMC_ADMIN_CAMUNDA_GROUP = "cmc-admin";
+    private static final String CMC_ADMIN_CAMUNDA_GROUP = "cmc-admin";
+    private static final String PROBATE_ADMIN_CAMUNDA_GROUP = "probate-admin";
+    private static final String PROBATE_ADMIN_AD_GROUP = "c43232cc-8f6d-4910-8bd1-47947f7c9a44";
+    private static final String PROBATE_TENANT_ID = "probate";
+    private static final String DEFAULT_GROUP = "default";
 
     @Override
     @SuppressWarnings("unchecked")
     public AuthenticationResult extractAuthenticatedUser(HttpServletRequest request, ProcessEngine engine) {
+        AuthorizationHelper authorizationHelper = new AuthorizationHelper(engine.getAuthorizationService());
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -63,10 +71,41 @@ public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenti
 
         @SuppressWarnings("unchecked")
         List<String> adGroups = (List<String>) attributes.getOrDefault("groups", emptyList());
-        
+
         authenticationResult.setTenants(getTenantsAndProvision(id, adGroups, identityService));
         authenticationResult.setGroups(getCamundaGroupsAndProvision(id, adGroups, identityService));
 
+        AuthorizationService authorizationService = engine.getAuthorizationService();
+        Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+
+        boolean cmcAdminAuthExists = authorizationService.createAuthorizationQuery().resourceType(Resources.APPLICATION)
+                .groupIdIn(CMC_ADMIN_CAMUNDA_GROUP).resourceId("*").count() > 0;
+        if (!cmcAdminAuthExists) {
+//            authorization.setGroupId(CMC_ADMIN_CAMUNDA_GROUP);
+//            authorization.addPermission(Permissions.ACCESS);
+//            authorization.setResource(Resources.APPLICATION);
+//            authorization.setResourceId("*");
+//            authorizationService.saveAuthorization(authorization);
+        }
+
+        authorizationHelper.deploymentAccess(CMC_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.taskAccess(CMC_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.processDefinition(CMC_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.processInstance(CMC_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.batchAccess(CMC_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.decisionDefinitionAccess(CMC_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.optimiseAccess(CMC_ADMIN_CAMUNDA_GROUP);
+
+        authorizationHelper.deploymentAccess(PROBATE_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.taskAccess(PROBATE_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.processDefinition(PROBATE_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.processInstance(PROBATE_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.batchAccess(PROBATE_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.decisionDefinitionAccess(PROBATE_ADMIN_CAMUNDA_GROUP);
+        authorizationHelper.optimiseAccess(PROBATE_ADMIN_CAMUNDA_GROUP);
+
+        authorizationHelper.cockpitAccess(DEFAULT_GROUP);
+        authorizationHelper.tasklistAccess(DEFAULT_GROUP);
 
         return authenticationResult;
     }
@@ -84,8 +123,24 @@ public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenti
             if (identityService.createTenantQuery()
                     .tenantId(CMC_TENANT_ID)
                     .userMember(id)
-                    .count() < 1) {
+                    .count() == 0) {
                 identityService.createTenantUserMembership(CMC_TENANT_ID, id);
+            }
+        }
+
+        if (adGroups.contains(PROBATE_ADMIN_AD_GROUP)) {
+            camundaTenants.add(PROBATE_TENANT_ID);
+            if (identityService.createTenantQuery().tenantId(PROBATE_TENANT_ID).count() == 0) {
+                Tenant tenant = identityService.newTenant(PROBATE_TENANT_ID);
+                tenant.setName("Probate");
+                identityService.saveTenant(tenant);
+            }
+
+            if (identityService.createTenantQuery()
+                    .tenantId(PROBATE_TENANT_ID)
+                    .userMember(id)
+                    .count() == 0) {
+                identityService.createTenantUserMembership(PROBATE_TENANT_ID, id);
             }
         }
 
@@ -99,19 +154,55 @@ public class SpringSecurityAuthenticationProvider extends ContainerBasedAuthenti
         }
 
         if (adGroups.contains(CMC_ADMIN_AD_GROUP)) {
-            if (identityService.createGroupQuery().groupId(CMC_ADMIN_CAMUNDA_GROUP).count() < 1) {
+            if (identityService.createGroupQuery().groupId(CMC_ADMIN_CAMUNDA_GROUP).count() == 0) {
                 Group group = identityService.newGroup(CMC_ADMIN_CAMUNDA_GROUP);
                 group.setName("CMC Admin");
                 identityService.saveGroup(group);
             }
 
             if (identityService.createTenantQuery()
-                            .tenantId(CMC_TENANT_ID)
-                            .groupMember(CMC_ADMIN_CAMUNDA_GROUP)
-                            .count() < 1) {
+                    .tenantId(CMC_TENANT_ID)
+                    .groupMember(CMC_ADMIN_CAMUNDA_GROUP)
+                    .count() == 0) {
                 identityService.createTenantGroupMembership(CMC_TENANT_ID, CMC_ADMIN_CAMUNDA_GROUP);
+
             }
+
+//            if (identityService.createUserQuery().memberOfGroup(CMC_ADMIN_CAMUNDA_GROUP).count() == 0) {
+                identityService.createMembership(id, CMC_ADMIN_CAMUNDA_GROUP);
+//            }
+            camundaGroups.add(CMC_ADMIN_CAMUNDA_GROUP);
         }
+
+        if (adGroups.contains(PROBATE_ADMIN_AD_GROUP)) {
+            if (identityService.createGroupQuery().groupId(PROBATE_ADMIN_CAMUNDA_GROUP).count() == 0) {
+                Group group = identityService.newGroup(PROBATE_ADMIN_CAMUNDA_GROUP);
+                group.setName("Probate Admin");
+                identityService.saveGroup(group);
+            }
+
+            if (identityService.createTenantQuery()
+                    .tenantId(PROBATE_TENANT_ID)
+                    .groupMember(PROBATE_ADMIN_CAMUNDA_GROUP)
+                    .count() == 0) {
+                identityService.createTenantGroupMembership(PROBATE_TENANT_ID, PROBATE_ADMIN_CAMUNDA_GROUP);
+            }
+
+//            if (identityService.createUserQuery().memberOfGroup(PROBATE_ADMIN_CAMUNDA_GROUP).count() == 0) {
+                identityService.createMembership(id, PROBATE_ADMIN_CAMUNDA_GROUP);
+//            }
+
+            camundaGroups.add(PROBATE_ADMIN_CAMUNDA_GROUP);
+        }
+
+        if (identityService.createGroupQuery().groupId(DEFAULT_GROUP).count() == 0) {
+            Group group = identityService.newGroup(DEFAULT_GROUP);
+            group.setName("All users");
+            identityService.saveGroup(group);
+        }
+
+        camundaGroups.add(DEFAULT_GROUP);
+
 
         return camundaGroups;
 
