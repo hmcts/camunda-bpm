8b71ffcf87265e76e3b11120787149975f4df372
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-07-31 21:59:11 +0100

    simplifying config

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.camunda.bpm.config;
 
 import org.camunda.bpm.webapp.impl.security.auth.ContainerBasedAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +16,7 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepo
 import java.util.Collections;
 
 @Configuration
-@ConditionalOnProperty(prefix = "security", name = "enabled", matchIfMissing = true)
+@ConditionalOnProperty(prefix = "spring.security", name = "enabled", matchIfMissing = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -30,30 +29,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private OAuth2AuthorizedClientService authorizedClientService;
 
-    @Value("${security.anonymous:false}")
-    private boolean allowAnonymous;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        if (allowAnonymous) {
-            http.csrf().disable()
-                .authorizeRequests().anyRequest().anonymous().and().httpBasic().disable();
-        } else {
-            http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/health", "/health/liveness").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login()
-                .clientRegistrationRepository(this.clientRegistrationRepository)
-                .authorizedClientRepository(this.authorizedClientRepository)
-                .authorizedClientService(this.authorizedClientService);
-        }
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/health", "/health/liveness").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2Login()
+            .clientRegistrationRepository(this.clientRegistrationRepository)
+            .authorizedClientRepository(this.authorizedClientRepository)
+            .authorizedClientService(this.authorizedClientService);
     }
 
     @Bean
-    @ConditionalOnProperty(prefix = "security", name = "anonymous", havingValue = "false", matchIfMissing = true)
     public FilterRegistrationBean containerBasedAuthenticationFilter() {
 
         FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
