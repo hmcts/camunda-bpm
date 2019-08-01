b142fe3d7b98ff8a8a354fc2b1903bf29a5de184
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-08-01 12:44:02 +0100

    remove imports

--- src/smokeTest/java/uk/gov/hmcts/reform/camunda.bpm/ApplicationHealthTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/camunda.bpm/ApplicationHealthTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.camunda.bpm;
 
-import io.restassured.RestAssured;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,7 +7,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
