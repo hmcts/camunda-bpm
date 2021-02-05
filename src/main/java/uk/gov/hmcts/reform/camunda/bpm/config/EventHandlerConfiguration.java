1640b1623323e403926055e7d4f32d5ea199b988
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-02-05 11:47:53 +0000

    Add feature flag

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.camunda.bpm.config;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.camunda.bpm.services.TaskConfigurationService;
@@ -8,13 +9,16 @@ import uk.gov.hmcts.reform.camunda.bpm.services.TaskConfigurationService;
 @Component
 class EventHandlerConfiguration {
 
+    private static final String TEST_PURPOSES_ASSIGNEE_ID = "demo";
+
     /*
      This assignee id is used as a blacklist
      to ignore tasks created with this assignee usually created for testing purposes
      */
-
-    private static final String TEST_PURPOSES_ASSIGNEE_ID = "demo";
     private final TaskConfigurationService taskConfigurationService;
+
+    @Value("${configuration.autoConfigureTasks}")
+    private boolean autoConfigureTaskEnabled;
 
     public EventHandlerConfiguration(TaskConfigurationService taskConfigurationService) {
         this.taskConfigurationService = taskConfigurationService;
@@ -23,9 +27,9 @@ class EventHandlerConfiguration {
     @EventListener
     public void onTaskCreatedEvent(DelegateTask delegateTask) {
 
-        // Avoid the first 2 demo tasks that get created for testing purposes on application startup
-        if (!TEST_PURPOSES_ASSIGNEE_ID.equals(delegateTask.getAssignee())) {
-
+        if (autoConfigureTaskEnabled) {
+            // Avoid the first 2 demo tasks that get created for testing purposes on application startup
+            if (!TEST_PURPOSES_ASSIGNEE_ID.equals(delegateTask.getAssignee())) {
 
             /*
              Uses DelegateTask as it is a mutable object
@@ -33,11 +37,9 @@ class EventHandlerConfiguration {
              The reason it is done in this way is because the tasks does not yet exist in the database
              when this event is triggered
              */
-
-
-            taskConfigurationService.configureTask(delegateTask);
+                taskConfigurationService.configureTask(delegateTask);
+            }
         }
-
     }
 
 }
