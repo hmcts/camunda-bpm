a041197081262c52d1169e486d7ad639cdbf2f1e
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2021-02-10 09:40:33 +0000

    Added event trigger check (#113)

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
@@ -1,22 +1,26 @@
 package uk.gov.hmcts.reform.camunda.bpm.config;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.camunda.bpm.services.TaskConfigurationService;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+
 @Component
 class EventHandlerConfiguration {
 
     private static final String TEST_PURPOSES_ASSIGNEE_ID = "demo";
-
+    private static final String CREATE_EVENT = "create";
+    private static final Logger LOG = getLogger(EventHandlerConfiguration.class);
     /*
      This assignee id is used as a blacklist
      to ignore tasks created with this assignee usually created for testing purposes
      */
     private final TaskConfigurationService taskConfigurationService;
-
     @Value("${configuration.autoConfigureTasks}")
     private boolean autoConfigureTaskEnabled;
 
@@ -29,7 +33,13 @@ class EventHandlerConfiguration {
 
         if (autoConfigureTaskEnabled) {
             // Avoid the first 2 demo tasks that get created for testing purposes on application startup
-            if (!TEST_PURPOSES_ASSIGNEE_ID.equals(delegateTask.getAssignee())) {
+            if (!TEST_PURPOSES_ASSIGNEE_ID.equals(delegateTask.getAssignee())
+                && CREATE_EVENT.equals(delegateTask.getEventName())) {
+
+                LOG.info(
+                    "Create event received, attempting to configure task with id: {}",
+                    delegateTask.getId()
+                );
 
                 /*
                  Uses DelegateTask as it is a mutable object
