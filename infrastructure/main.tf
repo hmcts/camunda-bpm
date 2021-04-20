cd96117d939d4de6854634ae9dddff80822c7e9e
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-04-20 08:07:26 +0100

    revert appinsights

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -54,30 +54,3 @@ resource "azurerm_key_vault_secret" "camunda_bpm_s2s_secret" {
   value        = data.azurerm_key_vault_secret.s2s_secret.value
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
-
-# Application insight
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.component}-appinsights-${var.env}"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
-
-  tags = var.common_tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
-}
-output "appInsightsInstrumentationKey" {
-  value = azurerm_application_insights.appinsights.instrumentation_key
-}
-
-resource "azurerm_key_vault_secret" "app_insights_key" {
-  name         = "AppInsightsInstrumentationKey"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
