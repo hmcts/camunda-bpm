119521581cbfc5e1f93644566e9c30e91d9a599f
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-05-12 09:11:03 +0100

    fix interpolation

--- infrastructure/elasticsearch.tf
+++ infrastructure/elasticsearch.tf
@@ -1,42 +1,42 @@
 provider "azurerm" {
   alias                      = "aks-infra"
-  subscription_id            = "${var.aks_infra_subscription_id}"
+  subscription_id            = var.aks_infra_subscription_id
   skip_provider_registration = true
   features {}
 }
 
 provider "azurerm" {
   alias           = "mgmt"
-  subscription_id = "${var.mgmt_subscription_id}"
+  subscription_id = var.mgmt_subscription_id
   features {}
 }
 
 module "elastic" {
   source                        = "git@github.com:hmcts/cnp-module-elk.git?ref=7.11.1"
-  product                       = "${var.raw_product}"
-  location                      = "${var.location}"
-  env                           = "${var.env}"
-  subscription                  = "${var.subscription}"
-  common_tags                   = "${var.common_tags}"
-  dataNodesAreMasterEligible    = "${var.dataNodesAreMasterEligible}"
-  vmDataNodeCount               = "${var.vmDataNodeCount}"
-  vmSizeAllNodes                = "${var.vmSizeAllNodes}"
-  storageAccountType            = "${var.storageAccountType}"
-  vmDataDiskCount               = "${var.vmDataDiskCount}"
-  ssh_elastic_search_public_key = "${data.azurerm_key_vault_secret.camunda_elastic_search_public_key.value}"
+  product                       = var.raw_product
+  location                      = var.location
+  env                           = var.env
+  subscription                  = var.subscription
+  common_tags                   = var.common_tags
+  dataNodesAreMasterEligible    = var.dataNodesAreMasterEligible
+  vmDataNodeCount               = var.vmDataNodeCount
+  vmSizeAllNodes                = var.vmSizeAllNodes
+  storageAccountType            = var.storageAccountType
+  vmDataDiskCount               = var.vmDataDiskCount
+  ssh_elastic_search_public_key = data.azurerm_key_vault_secret.camunda_elastic_search_public_key.value
   providers = {
     azurerm           = azurerm
     azurerm.mgmt      = azurerm.mgmt
     azurerm.aks-infra = azurerm.aks-infra
   }
-  logAnalyticsId      = "${data.azurerm_log_analytics_workspace.log_analytics.workspace_id}"
-  logAnalyticsKey     = "${data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key}"
-  dynatrace_instance  = "${var.dynatrace_instance}"
-  dynatrace_hostgroup = "${var.dynatrace_hostgroup}"
-  dynatrace_token     = "${data.azurerm_key_vault_secret.dynatrace_token.value}"
+  logAnalyticsId      = data.azurerm_log_analytics_workspace.log_analytics.workspace_id
+  logAnalyticsKey     = data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key
+  dynatrace_instance  = var.dynatrace_instance
+  dynatrace_hostgroup = var.dynatrace_hostgroup
+  dynatrace_token     = data.azurerm_key_vault_secret.dynatrace_token.value
   enable_logstash     = "No"
   enable_kibana       = "No"
-  alerts_email        = "${data.azurerm_key_vault_secret.alerts_email.value}"
+  alerts_email        = data.azurerm_key_vault_secret.alerts_email.value
 }
 
 locals {
@@ -51,27 +51,27 @@ data "azurerm_log_analytics_workspace" "log_analytics" {
 
 data "azurerm_key_vault_secret" "camunda_elastic_search_public_key" {
   name         = "${var.raw_product}-ELASTIC-SEARCH-PUB-KEY"
-  key_vault_id = "${module.vault.key_vault_id}"
+  key_vault_id = module.vault.key_vault_id
 }
 
 data "azurerm_key_vault_secret" "dynatrace_token" {
   name         = "dynatrace-token"
-  key_vault_id = "${module.vault.key_vault_id}"
+  key_vault_id = module.vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "elastic_search_url_key_setting" {
   name         = "${var.raw_product}-ELASTIC-SEARCH-URL"
-  value        = "${module.elastic.loadbalancerManual}"
-  key_vault_id = "${module.vault.key_vault_id}"
+  value        = module.elastic.loadbalancerManual
+  key_vault_id = module.vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "elastic_search_pwd_key_setting" {
   name         = "${var.raw_product}-ELASTIC-SEARCH-PASSWORD"
-  value        = "${module.elastic.elasticsearch_admin_password}"
-  key_vault_id = "${module.vault.key_vault_id}"
+  value        = module.elastic.elasticsearch_admin_password
+  key_vault_id = module.vault.key_vault_id
 }
 
 data "azurerm_key_vault_secret" "alerts_email" {
   name         = "alerts-email"
-  key_vault_id = "${module.vault.key_vault_id}"
+  key_vault_id = module.vault.key_vault_id
 }
