bd477d56cc8d5c2a484ea2e85b4ed73423d83211
jordankainos <j.brown@kainos.com>
2021-06-16 11:55:54 +0100

    Removing example-process (#178)

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/CamundaApplication.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/CamundaApplication.java
@@ -3,14 +3,12 @@ package uk.gov.hmcts.reform.camunda.bpm;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableProcessApplication("example-processs")
+@EnableProcessApplication
 @EnableFeignClients(basePackages =
     {
         "uk.gov.hmcts.reform.authorisation",
@@ -23,12 +21,5 @@ public class CamundaApplication {
 
     public static void main(String... args) {
         SpringApplication.run(CamundaApplication.class, args);
-    }
-
-    // This is only used for testing purposes
-    @Bean
-    public CommandLineRunner createDemoProcessInstance() {
-        return (String[] args) -> runtimeService.startProcessInstanceByKey("example-processs");
-
     }
 }
