fcbf20d91bc582550c4a1c3387d9150c8bb9c474
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-06-30 12:04:10 +0100

    Added Event listener condition and added more events for delete and complete to add a new local variable

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
@@ -13,13 +13,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Component
 class EventHandlerConfiguration {
 
-    private static final String TEST_PURPOSES_ASSIGNEE_ID = "demo";
-    private static final String CREATE_EVENT = "create";
     private static final Logger LOG = getLogger(EventHandlerConfiguration.class);
-    /*
-     This assignee id is used as a blacklist
-     to ignore tasks created with this assignee usually created for testing purposes
-     */
     private final TaskConfigurationService taskConfigurationService;
     @Value("${configuration.autoConfigureTasks}")
     private boolean autoConfigureTaskEnabled;
@@ -28,33 +22,38 @@ class EventHandlerConfiguration {
         this.taskConfigurationService = taskConfigurationService;
     }
 
-    @EventListener
+    @EventListener(condition = "#delegateTask.eventName=='create'")
     public void onTaskCreatedEvent(DelegateTask delegateTask) {
+        if (autoConfigureTaskEnabled) {
+            LOG.info(
+                "Create event received, attempting to configure task with id: {}",
+                delegateTask.getId()
+            );
+            /*
+            Uses DelegateTask as it is a mutable object
+            Call wa-task-configuration to retrieve configuration for a tasks.
+            The reason it is done in this way is because the tasks does not yet exist in the database
+            when this event is triggered
+            */
 
-        if (CREATE_EVENT.equals(delegateTask.getEventName())) {
-            if (autoConfigureTaskEnabled) {
-                // Avoid the first 2 demo tasks that get created for testing purposes on application startup
-                if (!TEST_PURPOSES_ASSIGNEE_ID.equals(delegateTask.getAssignee())) {
-
-                    LOG.info(
-                        "Create event received, attempting to configure task with id: {}",
-                        delegateTask.getId()
-                    );
-
-                    /*
-                     Uses DelegateTask as it is a mutable object
-                     Call wa-task-configuration to retrieve configuration for a tasks.
-                     The reason it is done in this way is because the tasks does not yet exist in the database
-                     when this event is triggered
-                     */
-
-                    taskConfigurationService.configureTask(delegateTask);
-                }
-            } else {
-                LOG.info(
-                    "Create event received. Event processed but not handled. auto configuration flag was disabled"
-                );
-            }
+            taskConfigurationService.configureTask(delegateTask);
+        } else {
+            LOG.info(
+                "Create event received. Event processed but not handled. auto configuration flag was disabled"
+            );
         }
     }
+
+    @EventListener(condition = "#delegateTask.eventName=='complete'")
+    public void onTaskCompletedEvent(DelegateTask delegateTask) {
+        LOG.info("Complete event received for task with id: {}", delegateTask.getId());
+        delegateTask.setVariableLocal("cftTaskState", "pendingTermination");
+    }
+
+    @EventListener(condition = "#delegateTask.eventName=='delete'")
+    public void onTaskDeletedEvent(DelegateTask delegateTask) {
+        LOG.info("Delete event received for task with id: {}", delegateTask.getId());
+        delegateTask.setVariableLocal("cftTaskState", "pendingTermination");
+    }
+
 }
---
+++ src/test/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfigurationTest.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.camunda.bpm.config;
+
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.impl.persistence.entity.TaskEntity;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.reform.camunda.bpm.services.TaskConfigurationService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EventHandlerConfigurationTest {
+
+    private EventHandlerConfiguration eventHandlerConfiguration;
+
+    @Mock
+    private TaskConfigurationService taskConfigurationService;
+
+    @Before
+    public void setUp() {
+
+        eventHandlerConfiguration = new EventHandlerConfiguration(taskConfigurationService);
+    }
+
+    @Test
+    public void onTaskCreatedEvent_should_call_task_configuration_service_when_flag_is_enabled() {
+
+        DelegateTask delegateTask = mock(DelegateTask.class);
+        when(delegateTask.getEventName()).thenReturn("create");
+        ReflectionTestUtils.setField(eventHandlerConfiguration, "autoConfigureTaskEnabled", true);
+        eventHandlerConfiguration.onTaskCreatedEvent(delegateTask);
+        verify(taskConfigurationService, times(1)).configureTask(delegateTask);
+    }
+
+
+    @Test
+    public void onTaskCreatedEvent_should_not_call_task_configuration_service_when_flag_is_disabled() {
+
+        TaskEntity delegateTask = mock(TaskEntity.class);
+        when(delegateTask.getEventName()).thenReturn("create");
+        ReflectionTestUtils.setField(eventHandlerConfiguration, "autoConfigureTaskEnabled", false);
+        eventHandlerConfiguration.onTaskCreatedEvent(delegateTask);
+        verify(taskConfigurationService, times(0)).configureTask(delegateTask);
+    }
+}
