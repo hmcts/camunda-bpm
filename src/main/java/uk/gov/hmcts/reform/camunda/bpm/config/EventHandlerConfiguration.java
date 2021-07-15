2c71d43fae5ba733f09e324ef769c52ba079563e
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2021-07-15 14:30:12 +0100

    RWA-582: Completion / Cancellation: Update Event Bridge to add task variable (#181)

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
@@ -13,13 +13,8 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Component
 class EventHandlerConfiguration {
 
-    private static final String TEST_PURPOSES_ASSIGNEE_ID = "demo";
-    private static final String CREATE_EVENT = "create";
     private static final Logger LOG = getLogger(EventHandlerConfiguration.class);
-    /*
-     This assignee id is used as a blacklist
-     to ignore tasks created with this assignee usually created for testing purposes
-     */
+    private static final String EVENT_RECEIVED_LOGGER_MESSAGE = "{} event received for task with id: {}";
     private final TaskConfigurationService taskConfigurationService;
     @Value("${configuration.autoConfigureTasks}")
     private boolean autoConfigureTaskEnabled;
@@ -28,33 +23,39 @@ class EventHandlerConfiguration {
         this.taskConfigurationService = taskConfigurationService;
     }
 
-    @EventListener
+    @EventListener(condition = "#delegateTask.eventName=='create'")
     public void onTaskCreatedEvent(DelegateTask delegateTask) {
+        if (autoConfigureTaskEnabled) {
+            LOG.info(EVENT_RECEIVED_LOGGER_MESSAGE, "CREATE", delegateTask.getId());
+            /*
+            Uses DelegateTask as it is a mutable object
+            Call wa-task-configuration to retrieve configuration for a tasks.
+            The reason it is done in this way is because the tasks does not yet exist in the database
+            when this event is triggered
+            */
 
-        if (CREATE_EVENT.equals(delegateTask.getEventName())) {
-            if (autoConfigureTaskEnabled) {
-                // Avoid the first 2 demo tasks that get created for testing purposes on application startup
-                if (!TEST_PURPOSES_ASSIGNEE_ID.equals(delegateTask.getAssignee())) {
+            taskConfigurationService.configureTask(delegateTask);
+        } else {
+            LOG.info(
+                EVENT_RECEIVED_LOGGER_MESSAGE
+                + " Event processed but not handled. Auto-configuration flag was disabled",
+                "CREATE",
+                delegateTask.getId()
+            );
 
-                    LOG.info(
-                        "Create event received, attempting to configure task with id: {}",
-                        delegateTask.getId()
-                    );
-
-                    /*
-                     Uses DelegateTask as it is a mutable object
-                     Call wa-task-configuration to retrieve configuration for a tasks.
-                     The reason it is done in this way is because the tasks does not yet exist in the database
-                     when this event is triggered
-                     */
-
-                    taskConfigurationService.configureTask(delegateTask);
-                }
-            } else {
-                LOG.info(
-                    "Create event received. Event processed but not handled. auto configuration flag was disabled"
-                );
-            }
         }
     }
+
+    @EventListener(condition = "#delegateTask.eventName=='complete'")
+    public void onTaskCompletedEvent(DelegateTask delegateTask) {
+        LOG.info(EVENT_RECEIVED_LOGGER_MESSAGE, "COMPLETE", delegateTask.getId());
+        delegateTask.setVariableLocal("cftTaskState", "pendingTermination");
+    }
+
+    @EventListener(condition = "#delegateTask.eventName=='delete'")
+    public void onTaskDeletedEvent(DelegateTask delegateTask) {
+        LOG.info(EVENT_RECEIVED_LOGGER_MESSAGE, "DELETE", delegateTask.getId());
+        delegateTask.setVariableLocal("cftTaskState", "pendingTermination");
+    }
+
 }
