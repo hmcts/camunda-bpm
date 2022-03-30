7b0b47b22976e980071318705e8743bc1faa1551
Ben Neill <benjaminrneill@gmail.com>
2022-03-30 13:01:44 +0100

    Update db.tf

--- infrastructure/db.tf
+++ infrastructure/db.tf
@@ -6,7 +6,7 @@ module "database" {
   postgresql_user    = "camundaadmin"
   database_name      = "camunda"
   postgresql_version = "11"
-  sku_name           = "GP_Gen5_2"
+  sku_name           = "GP_Gen5_4"
   sku_tier           = "GeneralPurpose"
   storage_mb         = "51200"
   common_tags        = var.common_tags
