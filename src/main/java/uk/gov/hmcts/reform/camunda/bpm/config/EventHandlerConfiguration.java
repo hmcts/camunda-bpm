37d9242a06dfcbb504572eb0bb8c483c8151f9f9
Gareth Sullivan <68859044+sullivangareth@users.noreply.github.com>
2022-10-12 13:18:40 +0100

    RWA-1522 - Remove task configuration code (#379)
    * RWA-1522 - Remove task configuration code
    
    * RWA-1522 - Temporarily add CVE suppression
    
    * RWA-1522 - Reinstate setting of task state on creation
    
    * Suppressing CVE-2022-42003, CVE-2022-42004
    
    Co-authored-by: rafalkalita <rkalitainfo@gmail.com>

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfiguration.java
@@ -2,13 +2,10 @@ package uk.gov.hmcts.reform.camunda.bpm.config;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.camunda.bpm.services.TaskConfigurationService;
 
 import static org.slf4j.LoggerFactory.getLogger;
-
 
 @Component
 class EventHandlerConfiguration {
@@ -16,38 +13,12 @@ class EventHandlerConfiguration {
     private static final Logger LOG = getLogger(EventHandlerConfiguration.class);
     private static final String EVENT_RECEIVED_LOGGER_MESSAGE = "{} event received for task with id: {}";
     private static final String CFT_TASK_STATE_LOCAL_VARIABLE_NAME = "cftTaskState";
-    private final TaskConfigurationService taskConfigurationService;
-    @Value("${configuration.autoConfigureTasks}")
-    private boolean autoConfigureTaskEnabled;
-
-    public EventHandlerConfiguration(TaskConfigurationService taskConfigurationService) {
-        this.taskConfigurationService = taskConfigurationService;
-    }
 
     @EventListener(condition = "#delegateTask.eventName=='create'")
     public void onTaskCreatedEvent(DelegateTask delegateTask) {
-        if (autoConfigureTaskEnabled) {
-            LOG.info(EVENT_RECEIVED_LOGGER_MESSAGE, "CREATE", delegateTask.getId());
-            /*
-            Uses DelegateTask as it is a mutable object
-            Call wa-task-configuration to retrieve configuration for a tasks.
-            The reason it is done in this way is because the tasks does not yet exist in the database
-            when this event is triggered
-            */
-
-            taskConfigurationService.configureTask(delegateTask);
-        } else {
-            LOG.info(
-                EVENT_RECEIVED_LOGGER_MESSAGE
-                + " Event processed but not handled. Auto-configuration flag was disabled",
-                "CREATE",
-                delegateTask.getId()
-            );
-
-        }
         LOG.info("Setting {} state to unconfigured for Task id: {}",
-            CFT_TASK_STATE_LOCAL_VARIABLE_NAME,
-            delegateTask.getId());
+                 CFT_TASK_STATE_LOCAL_VARIABLE_NAME,
+                 delegateTask.getId());
         delegateTask.setVariableLocal(CFT_TASK_STATE_LOCAL_VARIABLE_NAME, "unconfigured");
     }
 
--- src/test/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/camunda/bpm/config/EventHandlerConfigurationTest.java
@@ -4,10 +4,7 @@ import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.test.util.ReflectionTestUtils;
-import uk.gov.hmcts.reform.camunda.bpm.services.TaskConfigurationService;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -19,32 +16,17 @@ public class EventHandlerConfigurationTest {
     private EventHandlerConfiguration eventHandlerConfiguration;
     private static final String CFT_TASK_STATE_LOCAL_VARIABLE_NAME = "cftTaskState";
 
-    @Mock
-    private TaskConfigurationService taskConfigurationService;
-
     @Before
     public void setUp() {
 
-        eventHandlerConfiguration = new EventHandlerConfiguration(taskConfigurationService);
+        eventHandlerConfiguration = new EventHandlerConfiguration();
     }
 
     @Test
-    public void onTaskCreatedEvent_should_call_task_configuration_service_when_flag_is_enabled() {
+    public void onTaskCreatedEvent_should_set_task_state_to_unconfigured() {
 
         DelegateTask delegateTask = mock(DelegateTask.class);
-        ReflectionTestUtils.setField(eventHandlerConfiguration, "autoConfigureTaskEnabled", true);
         eventHandlerConfiguration.onTaskCreatedEvent(delegateTask);
-        verify(taskConfigurationService, times(1)).configureTask(delegateTask);
-        verify(delegateTask, times(1)).setVariableLocal(CFT_TASK_STATE_LOCAL_VARIABLE_NAME, "unconfigured");
-    }
-
-    @Test
-    public void onTaskCreatedEvent_should_not_call_task_configuration_service_when_flag_is_disabled() {
-
-        DelegateTask delegateTask = mock(DelegateTask.class);
-        ReflectionTestUtils.setField(eventHandlerConfiguration, "autoConfigureTaskEnabled", false);
-        eventHandlerConfiguration.onTaskCreatedEvent(delegateTask);
-        verify(taskConfigurationService, times(0)).configureTask(delegateTask);
         verify(delegateTask, times(1)).setVariableLocal(CFT_TASK_STATE_LOCAL_VARIABLE_NAME, "unconfigured");
     }
 }
