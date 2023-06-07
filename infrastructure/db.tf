fa18c3f8b2fcb7cc869fe6559b1ede5c947091a4
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-07 09:15:31 +0100

    DTSPO-13692 - Delete single server (#562)

--- infrastructure/db.tf
+++ infrastructure/db.tf
@@ -1,49 +1,3 @@
-module "database" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = "hmcts-${var.product}"
-  location           = var.location
-  env                = var.env
-  postgresql_user    = "camundaadmin"
-  database_name      = "camunda"
-  postgresql_version = "11"
-  sku_name           = var.sku_name
-  sku_tier           = "GeneralPurpose"
-  storage_mb         = var.storage_mb
-  common_tags        = var.common_tags
-  subscription       = var.subscription
-  sku_capacity       = var.sku_capacity
-}
-
-resource "azurerm_key_vault_secret" "postgres-user" {
-  name         = "bpm-POSTGRES-USER"
-  value        = module.database.user_name
-  key_vault_id = module.vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "postgres-password" {
-  name         = "bpm-POSTGRES-PASS"
-  value        = module.database.postgresql_password
-  key_vault_id = module.vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "postgres-host" {
-  name         = "bpm-POSTGRES-HOST"
-  value        = module.database.host_name
-  key_vault_id = module.vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "postgres-port" {
-  name         = "bpm-POSTGRES-PORT"
-  value        = module.database.postgresql_listen_port
-  key_vault_id = module.vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "postgres-database" {
-  name         = "bpm-POSTGRES-DATABASE"
-  value        = module.database.postgresql_database
-  key_vault_id = module.vault.key_vault_id
-}
-
 module "postgresql_flexible" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
@@ -69,7 +23,6 @@ module "postgresql_flexible" {
   pgsql_firewall_rules = []
   pgsql_version = "14"
 }
-
 resource "azurerm_key_vault_secret" "postgres-user-v14" {
   name         = "bpm-v14-POSTGRES-USER"
   value        = module.postgresql_flexible.username
