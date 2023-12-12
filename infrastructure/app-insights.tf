b1096dcaf26e853f40d053a244c55467cea75735
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-12-12 10:02:09 +0000

    DTSPO-16178 moving App insight to use module

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -1,5 +1,28 @@
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env     = var.env
+  product = var.product
+  name    = "${var.product}-${var.component}-appinsights-${var.env}"
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  common_tags = var.common_tags
+}
+
+moved {
+  from = azurerm_application_insights.appinsights
+  to   = module.application_insights.azurerm_application_insights.this
+}
+
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
+  key_vault_id = module.vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "app_insights_key" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = module.application_insights.instrumentation_key
   key_vault_id = module.vault.key_vault_id
 }
