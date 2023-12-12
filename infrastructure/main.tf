3924ea5f3eb92bea9436afe2c0b4a960072ecc49
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-12-12 11:27:16 +0000

    DTSPO-16178 moving App insight to use module (#711)
    * DTSPO-16178 moving App insight to use module
    
    * supressed couple of CVEs
    
    * updated name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,28 +40,3 @@ resource "azurerm_key_vault_secret" "camunda_bpm_s2s_secret" {
   value        = data.azurerm_key_vault_secret.s2s_secret.value
   key_vault_id = module.vault.key_vault_id
 }
-
-
-# Application insight
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.component}-appinsights-${var.env}"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
-
-  tags = var.common_tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
-}
-
-resource "azurerm_key_vault_secret" "app_insights_key" {
-  name         = "AppInsightsInstrumentationKey"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
-  key_vault_id = module.vault.key_vault_id
-}
