67168ac31969ab7bdf7fa9fe4af399095097e043
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-12-14 10:36:53 +0000

    lint

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/WebSecurityWebAppConfig.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/config/WebSecurityWebAppConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityWebAppConfig {
     public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeRequests( requests -> requests.anyRequest().authenticated())
+                .authorizeRequests(requests -> requests.anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2.clientRegistrationRepository(this.clientRegistrationRepository)
                         .authorizedClientRepository(this.authorizedClientRepository)
                         .authorizedClientService(this.authorizedClientService));
