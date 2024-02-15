f68ff48ad2e4214171fa8929408c9a9ab4563eda
Marty Fox <martin.fox1@justice.gov.uk>
2024-02-15 08:47:22 +0000

    removing all code to test oauth redirect

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/exception/PendingMigrationScriptException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.camunda.bpm.exception;
-
-public class PendingMigrationScriptException extends RuntimeException {
-
-    public PendingMigrationScriptException(String script) {
-        super("Found migration not yet applied: " + script);
-    }
-}
