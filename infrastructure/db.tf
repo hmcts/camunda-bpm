3b0cd26aca810bc1ef745febcc868493dfcd7661
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-04-12 10:09:19 +0100

    Point postgres module to pr branch

--- infrastructure/db.tf
+++ infrastructure/db.tf
@@ -3,7 +3,7 @@ module "postgresql_flexible" {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=temp-disable-ha-camunda"
   env                  = var.env
   product              = var.product
   name                 = "hmcts-${var.product}-v14-flexible"
