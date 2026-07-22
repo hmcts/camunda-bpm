40153132202b35dfd2fb05f372e0d33aa29015f2
Daniel Alves <daniel.alves@justice.gov.uk>
2026-07-22 16:16:34 +0100

    feat: DTSPO-32488 Upgrade camunda-bpm to Java 25

--- src/test/java/uk/gov/hmcts/reform/camunda/bpm/filter/webapp/SpringSecurityWebappAuthenticationProviderTest.java
+++ src/test/java/uk/gov/hmcts/reform/camunda/bpm/filter/webapp/SpringSecurityWebappAuthenticationProviderTest.java
@@ -16,7 +16,6 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +27,7 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
@@ -73,13 +73,13 @@ public class SpringSecurityWebappAuthenticationProviderTest {
         })
         .around(postgreSQLContainer);
 
-    @MockBean
+    @MockitoBean
     private ClientRegistrationRepository clientRegistrationRepository;
 
-    @MockBean
+    @MockitoBean
     private OAuth2AuthorizedClientRepository authorizedClientRepository;
 
-    @MockBean
+    @MockitoBean
     private OAuth2AuthorizedClientService authorizedClientService;
 
 
@@ -92,7 +92,7 @@ public class SpringSecurityWebappAuthenticationProviderTest {
     @Autowired
     private IdentityService identityService;
 
-    @MockBean
+    @MockitoBean
     private ServiceAuthorisationApi serviceAuthorisationApi;
 
     @Test
