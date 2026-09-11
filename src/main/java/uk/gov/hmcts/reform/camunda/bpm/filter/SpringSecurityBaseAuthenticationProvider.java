0c82fe83e0439d1e5ac1a09c644b735327dbf7da
Praveen Adusumilli <praveen.adusumilli@justice.gov.uk>
2026-09-11 14:37:47 +0100

    revert debug

--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/filter/SpringSecurityBaseAuthenticationProvider.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/filter/SpringSecurityBaseAuthenticationProvider.java
@@ -4,8 +4,6 @@ import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.identity.Group;
 import org.camunda.bpm.engine.identity.Tenant;
 import org.camunda.bpm.engine.rest.security.auth.impl.ContainerBasedAuthenticationProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.reform.camunda.bpm.app.AuthorizationHelper;
 import uk.gov.hmcts.reform.camunda.bpm.config.AccessControl;
 import uk.gov.hmcts.reform.camunda.bpm.config.ConfigProperties;
@@ -17,31 +15,17 @@ import java.util.List;
 
 public class SpringSecurityBaseAuthenticationProvider extends ContainerBasedAuthenticationProvider {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SpringSecurityBaseAuthenticationProvider.class);
     public static final String DEFAULT_GROUP_NAME = "All users";
     private static final String CAMUNDA_ADMIN_GROUP = "camunda-admin";
     private static final String DEFAULT_GROUP = "default";
     protected ConfigProperties configProperties;
 
     protected void refreshAuthorisation(AuthorizationHelper authorizationHelper) {
-        LOG.debug("Refreshing Camunda authorizations for {} configured groups",
-            configProperties.getCamundaGroups().size());
         configProperties.getCamundaGroups().forEach((key, groupConfig) -> {
                 AccessControl accessControl = configProperties.getCamundaAccess().get(groupConfig.getAccessControl());
                 if (accessControl == null) {
-                    LOG.debug("Skipping authorization refresh for mapping key '{}', Camunda group '{}': "
-                            + "access-control profile '{}' is not configured",
-                        key, groupConfig.getGroupId(), groupConfig.getAccessControl());
                     return;
                 }
-                LOG.debug("Refreshing authorization for mapping key '{}', Camunda group '{}', tenant '{}', "
-                        + "access-control profile '{}' [deployment={}, task={}, processDefinition={}, "
-                        + "processInstance={}, batch={}, decisionDefinition={}, optimize={}]",
-                    key, groupConfig.getGroupId(), groupConfig.getTenantId(), groupConfig.getAccessControl(),
-                    accessControl.isDeploymentAccess(), accessControl.isTaskAccess(),
-                    accessControl.isProcessDefinition(), accessControl.isProcessInstance(),
-                    accessControl.isBatchAccess(), accessControl.isDecisionDefinitionAccess(),
-                    accessControl.isOptimiseAccess());
                 if (accessControl.isDeploymentAccess()) {
                     authorizationHelper.deploymentAccess(groupConfig.getGroupId());
                 }
@@ -70,35 +54,22 @@ public class SpringSecurityBaseAuthenticationProvider extends ContainerBasedAuth
     protected List<String> getTenantsAndProvision(String id, List<GroupConfig> applicableGroups,
                                                   IdentityService identityService) {
         List<String> camundaTenants = new ArrayList<>();
-        LOG.debug("Provisioning {} applicable Camunda tenant mappings for user '{}'",
-            applicableGroups.size(), id);
         applicableGroups.forEach(groupConfig -> {
                 camundaTenants.add(groupConfig.getTenantId());
-                long tenantCount = identityService.createTenantQuery().tenantId(groupConfig.getTenantId()).count();
-                LOG.debug("Tenant '{}' lookup returned {} existing records for user '{}'",
-                    groupConfig.getTenantId(), tenantCount, id);
-                if (tenantCount == 0) {
+                if (identityService.createTenantQuery().tenantId(groupConfig.getTenantId()).count() == 0) {
                     Tenant tenant = identityService.newTenant(groupConfig.getTenantId());
                     tenant.setName(groupConfig.getTenantId());
                     identityService.saveTenant(tenant);
-                    LOG.debug("Created Camunda tenant '{}' while provisioning user '{}'",
-                        groupConfig.getTenantId(), id);
                 }
 
-                long membershipCount = identityService.createTenantQuery()
+                if (identityService.createTenantQuery()
                     .tenantId(groupConfig.getTenantId())
                     .userMember(id)
-                    .count();
-                LOG.debug("Tenant '{}' membership lookup returned {} records for user '{}'",
-                    groupConfig.getTenantId(), membershipCount, id);
-                if (membershipCount == 0) {
+                    .count() == 0) {
                     identityService.createTenantUserMembership(groupConfig.getTenantId(), id);
-                    LOG.debug("Created membership between user '{}' and Camunda tenant '{}'",
-                        id, groupConfig.getTenantId());
                 }
             }
         );
-        LOG.debug("Finished tenant provisioning for user '{}'; authentication tenants={}", id, camundaTenants);
         return camundaTenants;
     }
 
@@ -106,75 +77,42 @@ public class SpringSecurityBaseAuthenticationProvider extends ContainerBasedAuth
                                                         IdentityService identityService) {
         List<String> camundaGroups = new ArrayList<>();
 
-        LOG.debug("Provisioning {} applicable Camunda group mappings for user '{}'; "
-                + "configured admin Entra group ID='{}'",
-            applicableGroups.size(), id, configProperties.getCamundaAdminGroupId());
         applicableGroups.forEach(groupConfig -> {
-                long groupCount = identityService.createGroupQuery().groupId(groupConfig.getGroupId()).count();
-                LOG.debug("Processing applicable mapping for user '{}': Entra group ID='{}', Camunda group='{}', "
-                        + "tenant='{}', adminGroupMatch={}; Camunda group lookup returned {} records",
-                    id, groupConfig.getAdGroupId(), groupConfig.getGroupId(), groupConfig.getTenantId(),
-                    groupConfig.getAdGroupId().equals(configProperties.getCamundaAdminGroupId()), groupCount);
-                if (groupCount == 0) {
+                if (identityService.createGroupQuery().groupId(groupConfig.getGroupId()).count() == 0) {
                     Group group = identityService.newGroup(groupConfig.getGroupId());
                     group.setName(groupConfig.getGroupId());
                     identityService.saveGroup(group);
-                    LOG.debug("Created Camunda group '{}' while provisioning user '{}'",
-                        groupConfig.getGroupId(), id);
                 }
 
-                long tenantGroupMembershipCount = identityService.createTenantQuery()
+                if (identityService.createTenantQuery()
                     .tenantId(groupConfig.getTenantId())
                     .groupMember(groupConfig.getGroupId())
-                    .count();
-                LOG.debug("Tenant '{}' to Camunda group '{}' membership lookup returned {} records",
-                    groupConfig.getTenantId(), groupConfig.getGroupId(), tenantGroupMembershipCount);
-                if (tenantGroupMembershipCount == 0) {
+                    .count() == 0) {
                     identityService
                         .createTenantGroupMembership(groupConfig.getTenantId(), groupConfig.getGroupId());
-                    LOG.debug("Created membership between Camunda tenant '{}' and group '{}'",
-                        groupConfig.getTenantId(), groupConfig.getGroupId());
                 }
                 identityService.createMembership(id, groupConfig.getGroupId());
                 camundaGroups.add(groupConfig.getGroupId());
-                LOG.debug("Created membership between user '{}' and Camunda group '{}'",
-                    id, groupConfig.getGroupId());
 
                 if (groupConfig.getAdGroupId().equals(configProperties.getCamundaAdminGroupId())) {
-                    long adminGroupCount = identityService.createGroupQuery().groupId(CAMUNDA_ADMIN_GROUP).count();
-                    LOG.debug("User '{}' matched configured admin Entra group ID '{}'; Camunda admin group lookup "
-                            + "returned {} records",
-                        id, groupConfig.getAdGroupId(), adminGroupCount);
-                    if (adminGroupCount == 0) {
+                    if (identityService.createGroupQuery().groupId(CAMUNDA_ADMIN_GROUP).count() == 0) {
                         Group group = identityService.newGroup(CAMUNDA_ADMIN_GROUP);
                         group.setName(CAMUNDA_ADMIN_GROUP);
                         identityService.saveGroup(group);
-                        LOG.debug("Created Camunda admin group '{}'", CAMUNDA_ADMIN_GROUP);
                     }
                     identityService.createMembership(id, CAMUNDA_ADMIN_GROUP);
                     camundaGroups.add(CAMUNDA_ADMIN_GROUP);
-                    LOG.debug("Granted Camunda admin membership to user '{}'", id);
-                } else {
-                    LOG.debug("Mapping did not grant Camunda admin membership to user '{}': Entra group ID '{}' "
-                            + "does not equal configured admin Entra group ID '{}'",
-                        id, groupConfig.getAdGroupId(), configProperties.getCamundaAdminGroupId());
                 }
             }
         );
 
-        long defaultGroupCount = identityService.createGroupQuery().groupId(DEFAULT_GROUP).count();
-        LOG.debug("Default Camunda group lookup returned {} records while provisioning user '{}'",
-            defaultGroupCount, id);
-        if (defaultGroupCount == 0) {
+        if (identityService.createGroupQuery().groupId(DEFAULT_GROUP).count() == 0) {
             Group group = identityService.newGroup(DEFAULT_GROUP);
             group.setName(DEFAULT_GROUP_NAME);
             identityService.saveGroup(group);
-            LOG.debug("Created default Camunda group '{}'", DEFAULT_GROUP);
         }
         identityService.createMembership(id, DEFAULT_GROUP);
         camundaGroups.add(DEFAULT_GROUP);
-        LOG.debug("Created default group membership for user '{}'; final authentication groups={}, isAdmin={}",
-            id, camundaGroups, camundaGroups.contains(CAMUNDA_ADMIN_GROUP));
 
         return camundaGroups;
 
--- src/main/java/uk/gov/hmcts/reform/camunda/bpm/filter/SpringSecurityWebappAuthenticationProvider.java
+++ src/main/java/uk/gov/hmcts/reform/camunda/bpm/filter/SpringSecurityWebappAuthenticationProvider.java
@@ -10,8 +10,6 @@ import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.authorization.Authorization;
 import org.camunda.bpm.engine.identity.User;
 import org.camunda.bpm.engine.rest.security.auth.AuthenticationResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -34,7 +32,6 @@ import static java.util.Objects.requireNonNull;
 @SuppressWarnings("unused")
 public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBaseAuthenticationProvider {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SpringSecurityWebappAuthenticationProvider.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final TypeReference<List<Object>> OBJECT_LIST_TYPE = new TypeReference<>() { };
     public static final String GIVEN_NAME = "given_name";
@@ -43,8 +40,6 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
     public static final String NAME = "name";
     public static final String UNIQUE_NAME = "unique_name";
     public static final String GROUPS_ATTRIBUTE = "groups";
-    private static final String CLAIM_NAMES_ATTRIBUTE = "_claim_names";
-    private static final String CLAIM_SOURCES_ATTRIBUTE = "_claim_sources";
     public static final String DEFAULT_GROUP_NAME = "All users";
     private static final String DEFAULT_GROUP = "default";
 
@@ -55,23 +50,13 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         configProperties = SpringContext.getAppContext().getBean(ConfigProperties.class);
-        LOG.debug("Starting Camunda webapp authentication extraction for request method='{}', URI='{}'; "
-                + "configuredMappings={}, configuredAdminEntraGroupId='{}'",
-            request.getMethod(), request.getRequestURI(), configProperties.getCamundaGroups().size(),
-            configProperties.getCamundaAdminGroupId());
 
         if (authentication == null) {
-            LOG.debug("Camunda webapp authentication unsuccessful: Spring Security context has no authentication");
             return AuthenticationResult.unsuccessful();
         }
 
         String id = authentication.getName();
-        LOG.debug("Found Spring Security authentication: type='{}', authenticated={}, principalName='{}', "
-                + "authorityCount={}",
-            authentication.getClass().getName(), authentication.isAuthenticated(), id,
-            authentication.getAuthorities().size());
         if (id == null || id.isEmpty()) {
-            LOG.debug("Camunda webapp authentication unsuccessful: principal name is null or empty");
             return AuthenticationResult.unsuccessful();
         }
 
@@ -81,18 +66,12 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
 
         if (!authorities.isEmpty()) {
             for (GrantedAuthority authority : authorities) {
-                LOG.debug("Inspecting authority type='{}', authority='{}' for user '{}'",
-                    authority.getClass().getName(), authority.getAuthority(), id);
                 if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
-                    LOG.debug("Merging OAuth2 authority attributes for user '{}'; attribute keys={}",
-                        id, oauth2UserAuthority.getAttributes().keySet());
                     attributes.putAll(oauth2UserAuthority.getAttributes());
                     id = authentication.getName();
                 }
             }
         }
-        LOG.debug("Finished extracting OAuth2 attributes for user '{}'; merged attribute keys={}",
-            id, attributes.keySet());
 
         final AuthenticationResult authenticationResult = new AuthenticationResult(
             id,
@@ -102,26 +81,9 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
         IdentityService identityService = engine.getIdentityService();
         updateUser(id, attributes, identityService);
 
-        Object rawGroupsClaim = attributes.get(GROUPS_ATTRIBUTE);
-        LOG.debug("Entra group claim diagnostics for user '{}': present={}, valueType='{}', groupCount={}, "
-                + "overageClaimNamesPresent={}, overageClaimSourcesPresent={}",
-            id, rawGroupsClaim != null,
-            rawGroupsClaim == null ? null : rawGroupsClaim.getClass().getName(),
-            rawGroupsClaim instanceof Collection<?> collection ? collection.size() : 0,
-            attributes.containsKey(CLAIM_NAMES_ATTRIBUTE), attributes.containsKey(CLAIM_SOURCES_ATTRIBUTE));
-        if (rawGroupsClaim == null && attributes.containsKey(CLAIM_NAMES_ATTRIBUTE)) {
-            LOG.debug("No direct groups claim was emitted for user '{}', but '_claim_names' is present (value={}). "
-                    + "This can indicate Entra group-claim overage; the application does not currently fetch the "
-                    + "groups from Microsoft Graph",
-                id, attributes.get(CLAIM_NAMES_ATTRIBUTE));
-        }
-
-        List<String> adGroups = normalizeGroupsClaim(rawGroupsClaim);
-        LOG.debug("Entra group IDs received for user '{}': {}", id, adGroups);
+        List<String> adGroups = normalizeGroupsClaim(attributes.get(GROUPS_ATTRIBUTE));
 
         List<GroupConfig> applicableGroups = getCamundaGroupsList(adGroups);
-        LOG.debug("Resolved {} applicable Camunda group mappings for user '{}': {}",
-            applicableGroups.size(), id, applicableGroups.stream().map(GroupConfig::getGroupId).toList());
 
         authenticationResult.setTenants(getTenantsAndProvision(id, applicableGroups, identityService));
         List<String> camundaGroups = getCamundaGroupsAndProvision(id, applicableGroups, identityService);
@@ -139,9 +101,6 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
         authorizationHelper.cockpitAccess(DEFAULT_GROUP);
         authorizationHelper.tasklistAccess(DEFAULT_GROUP);
 
-        LOG.debug("Camunda webapp authentication successful for user '{}'; groups={}, tenants={}, isAdmin={}",
-            id, authenticationResult.getGroups(), authenticationResult.getTenants(),
-            authenticationResult.getGroups().contains("camunda-admin"));
         return authenticationResult;
     }
 
@@ -169,8 +128,6 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
         }
 
         if (!(claimValue instanceof String stringValue)) {
-            LOG.debug("Ignoring unsupported value in Entra groups claim: valueType='{}'",
-                claimValue == null ? null : claimValue.getClass().getName());
             return;
         }
 
@@ -178,12 +135,10 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
         if (trimmedValue.startsWith("[") && trimmedValue.endsWith("]")) {
             try {
                 List<Object> decodedGroups = OBJECT_MAPPER.readValue(trimmedValue, OBJECT_LIST_TYPE);
-                LOG.debug("Decoded JSON-encoded Entra groups claim containing {} entries", decodedGroups.size());
                 decodedGroups.forEach(value -> addNormalizedGroups(value, normalizedGroups));
                 return;
             } catch (JsonProcessingException exception) {
-                LOG.debug("Entra groups claim value looked like a JSON array but could not be decoded; "
-                    + "retaining it as a scalar value", exception);
+                // Retain malformed JSON-like values as scalar group identifiers.
             }
         }
 
@@ -200,16 +155,10 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
         user.setEmail(requireNonNull(attributes.get(UNIQUE_NAME)).toString());
 
         User userExists = identityService.createUserQuery().userId(id).singleResult();
-        LOG.debug("Updating Camunda user '{}'; existingUser={}, givenNameClaimPresent={}, familyNameClaimPresent={}, "
-                + "displayNameClaimPresent={}, uniqueNameClaimPresent={}",
-            id, userExists != null, attributes.containsKey(GIVEN_NAME), attributes.containsKey(FAMILY_NAME),
-            attributes.containsKey(NAME), attributes.containsKey(UNIQUE_NAME));
         if (userExists != null) {
             identityService.deleteUser(id);
-            LOG.debug("Deleted existing Camunda user '{}' before refreshing its identity and memberships", id);
         }
         identityService.saveUser(user);
-        LOG.debug("Saved refreshed Camunda user '{}'", id);
     }
 
     private static String getFirstName(Map<String, Object> attributes, String name) {
@@ -242,22 +191,12 @@ public class SpringSecurityWebappAuthenticationProvider extends SpringSecurityBa
     private List<GroupConfig> getCamundaGroupsList(List<String> adGroups) {
         List<GroupConfig> applicableGroups = new ArrayList<>();
 
-        LOG.debug("Comparing {} Entra group IDs from the token against {} configured Camunda mappings",
-            adGroups.size(), configProperties.getCamundaGroups().size());
         configProperties.getCamundaGroups().forEach((key, groupConfig) -> {
-                boolean matches = adGroups.contains(groupConfig.getAdGroupId());
-                LOG.debug("Entra-to-Camunda mapping comparison: key='{}', configuredEntraGroupId='{}', "
-                        + "CamundaGroup='{}', tenant='{}', matchesTokenGroups={}, adminGroupConfigMatch={}",
-                    key, groupConfig.getAdGroupId(), groupConfig.getGroupId(), groupConfig.getTenantId(), matches,
-                    groupConfig.getAdGroupId().equals(configProperties.getCamundaAdminGroupId()));
-                if (matches) {
+                if (adGroups.contains(groupConfig.getAdGroupId())) {
                     applicableGroups.add(groupConfig);
                 }
             }
         );
-        if (applicableGroups.isEmpty()) {
-            LOG.debug("No configured Camunda mappings matched the Entra group IDs in the user's token");
-        }
         return applicableGroups;
     }
 
